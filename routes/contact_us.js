f5f622b21e47d0cf246a704db1947fbb41bd53f3
Maheen Contractor <maheencontractor@gmail.com>
2017-05-04 02:30:26 +0500

    db fetch commands added and nodemailer stuff added

--- routes/contact_us.js
+++ routes/contact_us.js
@@ -1,9 +1,44 @@
-var express = require('express')
+var express = require('express');
+var nodemailer = require('nodemailer');
+var mongodb = require('mongodb');
+var MongoClient = require('mongodb').MongoClient;
 var router = express.Router();
+var multer  = require('multer')
+var upload = multer({ dest: 'uploads/' })
 
 router.get('/', function(req, res, next) {
   res.render('contact_us', { title: 'Contact Us' });
 
+});
+
+// create reusable transporter object using the default SMTP transport
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'helpservice.bbs@gmail.com',
+        pass: 'bbsforlums2018'
+    }
+});
+
+router.post('/submit', upload.any(), function(req,res,next){
+	// setup email data with unicode symbols
+	let mailOptions = {
+	    from: 'helpservice.bbs@gmail.com',
+	    to: 'helpservice.bbs@gmail.com', // sender address 
+	    subject: 'Contact us message', // Subject line
+	    text: "The details of the contact us page are given below." +'\n'+ "Name : "+req.body.name+
+	    '\n'+ "Email ID : "+req.body.email+'\n'+"Contact Info : "+req.body.phone+'\n'+
+	    "Contact reason : "+req.body.type+ '\n'+ "Message : "+req.body.message+"\n"
+	};
+
+	// send mail with defined transport object
+	transporter.sendMail(mailOptions, (error, info) => {
+	    if (error) {
+	        return console.log(error);
+	    }
+	    console.log('Message %s sent: %s', info.messageId, info.response);
+	    res.redirect('/feedback');
+	});
 });
 
 module.exports = router;
