d39aa40e598ef59e77b6b517613ec4b2a91678e6
Maheen Contractor <maheencontractor@gmail.com>
2017-05-06 03:35:39 +0500

    login system fixed

--- routes/login2.js
+++ routes/login2.js
@@ -6,29 +6,4 @@ router.get('/', function(req, res, next) {
 
 });
 
-router.post('/submit',function(req,res,next){
-	var MongoClient = require('mongodb').MongoClient;
-	var url = 'mongodb://127.0.0.1:27017/BBS';
-
-	MongoClient.connect(url, function(err, db) {
-
-	    var cursor = db.collection('users').find({'email':req.body.email});
-
-	    cursor.each(function(err, doc) {
-	    	console.log(doc)
-	    	if(doc != null){
-	    		if(doc.email == req.body.email && doc.password == req.body.password){
-					res.redirect('/main');
-	    		}
-	    		else{
-					res.redirect('/login2');
-	    		}
-	    	}
-
-	    });
-	}); 	
-
-
-})
-
 module.exports = router;
