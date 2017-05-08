dab5623e8056c07a81a670a3c46e3e9c275a45be
saifay14 <18100014@lums.edu.pk>
2017-05-09 04:24:00 +0500

    borrowscreen updated

--- routes/borrowscreen.js
+++ routes/borrowscreen.js
@@ -1,15 +1,43 @@
 var express = require('express')
 var router = express.Router();
 
+/* GET home page. */
 router.get('/', function(req, res, next) {
 	var name12;
+	var json_reqs=[];
+	var val = req.query[1]
+	if (val == undefined)
+		val = 0;
+	var i = val*5;
+
     if ((require('./login.js').name12) != undefined ) {
       name12 = require('./login.js').name12;
     }
     else
       name12 = require('./signup.js').name12;
-  res.render('borrowscreen', { title: 'borrowscreen', name122:name12 });
 
+  var MongoClient = require('mongodb').MongoClient;
+	var url = 'mongodb://127.0.0.1:27017/BBS';
+
+		MongoClient.connect(url, function(err, db) {
+			if (err) {
+	        	console.log('Unable to connect to the Server:', err);
+	      	} else {
+	        console.log('Connected to Server');
+		    };
+
+		    var cursor = db.collection('requests').find();		   
+		    cursor.each(function(err, doc) {
+		    	if(doc != null){
+		    		json_reqs.push(doc);
+		   			
+		    	}
+		    	else {
+		    		console.log(json_reqs.length)
+		    		res.render('borrowscreen', { title: 'borrowscreen', name122:name12, array_1: json_reqs[0+i],array_2:json_reqs[1+i],array_3:json_reqs[2+i],array_4:json_reqs[3+i],array_5:json_reqs[4+i] });
+		    	}
+		    });
+		}); 
 });
 
 module.exports = router;
--- routes/upload_requests.js
+++ routes/upload_requests.js
@@ -9,7 +9,33 @@ router.get('/', function(req, res, next) {
     else
       name12 = require('./signup.js').name12;
   res.render('upload_requests', { title: 'Upload A Request',name122:name12 });
+});
 
+// Creating JSON Array for Requests.
+var json_reqs = [];
+router.get(function(req,res,next) {
+	var MongoClient = require('mongodb').MongoClient;
+	var url = 'mongodb://127.0.0.1:27017/BBS';
+
+	MongoClient.connect(url, function(err, db) {
+
+	    var cursor = db.collection('requests').find();
+	    // Execute the each command, triggers for each document
+		cursor.each( function(err, item) {
+			// If the item is null then the cursor is exhausted/empty and closed
+        	if(item != null) {
+	        	var obj = {
+	        		email: item.email,
+    			   	title: item.title,
+    			   	name: item.name,
+    			   	category: item.category,
+    			  	description: item.description,
+    			  	contact: item.contact
+	        	};
+	        	json_reqs.push(obj);
+	        };	        
+      	});
+	});   
 });
 
 router.post('/submit',function(req,res,next){
@@ -20,7 +46,7 @@ router.post('/submit',function(req,res,next){
 
 	    var cursor = db.collection('requests');
 	    var email = require('./login.js').email;
-	    var ad = {
+	    var reqs = {
 	    	email : email,
 	    	title : req.body.title,
 	    	name : req.body.pname,
@@ -31,12 +57,12 @@ router.post('/submit',function(req,res,next){
 	    };
 	    var req_name = req.body.pname;
 		module.exports.req_name = req_name;
-	    cursor.insert(ad);
+	    json_reqs.push(reqs);
+	    cursor.insert(reqs);
 	    db.close();
 	    res.redirect('/req_pic');
+	    module.exports.json_reqs = json_reqs;
 	}); 	
-
-
-})
+});
 
 module.exports = router;
